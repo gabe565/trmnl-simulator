9f3625e6d6b7f10487464b4fa0c76f84297cb012
Gabe Cook <gabe565@gmail.com>
2025-08-30 00:04:07 -0500

    perf: replace device frame with backgrounds, svgs, and inset shadows

--- src/components/DevicePreview.vue
+++ src/components/DevicePreview.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useFullscreen } from "@vueuse/core";
 import { ref } from "vue";
-import deviceImg from "@/assets/trmnl-device.png";
+import TrmnlLogo from "@/assets/trmnl-logo.svg?raw";
 import { Button } from "@/components/ui/button";
 import FullscreenIcon from "~icons/material-symbols/fullscreen-rounded";
 
@@ -12,26 +12,25 @@ const { toggle, isFullscreen } = useFullscreen(screenRef);
 </script>
 
 <template>
-  <div class="relative text-center rounded-lg mx-auto max-w-[900px]">
-    <Button
-      @click="toggle"
-      class="absolute z-10 right-0 bottom-0 m-2 inset-shadow-sm/15 shadow-none"
-    >
+  <div class="relative rounded-lg mx-auto max-w-[800px] bg-[#f7f7f7] p-[25px] pt-[24px] pb-[57px]">
+    <Button @click="toggle" class="absolute right-0 bottom-0 m-2 inset-shadow-sm/15 shadow-none">
       <FullscreenIcon />
     </Button>
-    <img
-      class="w-full rounded-lg select-none pointer-events-none"
-      :src="deviceImg"
-      alt="TRMNL Frame"
-      draggable="false"
-    />
-    <img
-      id="screen"
-      ref="screenRef"
-      :src="props.imageSrc"
-      alt="TRMNL Screen"
-      class="absolute inset-0 px-6 pt-5.5 pb-15"
-      :class="{ 'mix-blend-multiply': !isFullscreen }"
-    />
+    <div
+      class="pointer-events-none absolute bottom-0 left-0 w-full flex justify-center pb-4.5"
+      aria-hidden="true"
+    >
+      <div class="w-28 fill-[#bababa]" v-html="TrmnlLogo" />
+    </div>
+    <div
+      class="aspect-5/3 w-full bg-[#f7f7f7] rounded-sm overflow-hidden inset-shadow-[0_5px_70px_rgba(0,0,0,0.25)]"
+    >
+      <img
+        ref="screenRef"
+        :src="props.imageSrc"
+        alt="TRMNL Screen"
+        :class="{ 'mix-blend-multiply': !isFullscreen }"
+      />
+    </div>
   </div>
 </template>
