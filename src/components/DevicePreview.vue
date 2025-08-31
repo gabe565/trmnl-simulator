101d455c49ef15598f648cc2a7164cd7b0aa810b
Gabe Cook <gabe565@gmail.com>
2025-08-31 03:42:44 -0500

    feat: improve fullscreen button visibility

--- src/components/DevicePreview.vue
+++ src/components/DevicePreview.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useFullscreen } from "@vueuse/core";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import setupLogo from "@/assets/setup-logo.png";
 import TrmnlLogo from "@/assets/trmnl-logo.svg?raw";
 import { Button } from "@/components/ui/button";
@@ -14,17 +14,22 @@ const props = withDefaults(defineProps<{ imageSrc?: string }>(), { imageSrc: "" 
 
 const screenRef = ref<HTMLImageElement | null>(null);
 const { toggle, isFullscreen } = useFullscreen(screenRef);
+
+const color = computed(() => Colors[config.device]);
 </script>
 
 <template>
   <div
-    class="relative rounded-lg mx-auto max-w-[800px] p-[3.4%] pt-[3.3%] pb-[7.6%] transition-all"
-    :style="{ 'background-color': Colors[config.device]?.color }"
+    class="relative rounded-lg mx-auto max-w-[800px] p-[3.4%] pt-[3.3%] pb-[7.6%] transition-all overflow-hidden"
+    :style="{ 'background-color': color?.color }"
     @dblclick="toggle"
   >
     <Button
       @click="toggle"
-      class="absolute right-0 bottom-0 m-2 inset-shadow-sm/15 shadow-none bg-white/15 size-6 sm:size-9"
+      class="absolute right-0 bottom-0 size-6 sm:size-9 rounded-none rounded-tl-lg"
+      :style="{ color: color?.text }"
+      :class="{ 'mix-blend-difference': !color?.text }"
+      variant="ghost"
       size="icon"
     >
       <FullscreenIcon />
--- src/sdk/trmnl/colors.ts
+++ src/sdk/trmnl/colors.ts
@@ -3,6 +3,7 @@ export type Label = "White" | "Black" | "Clear" | "Sage" | "Gray" | "Wood";
 export interface Option {
   label: Label;
   color: string;
+  text?: string;
 }
 
 export const Colors: Readonly<Option[]> = Object.freeze([
@@ -10,6 +11,6 @@ export const Colors: Readonly<Option[]> = Object.freeze([
   { label: "Black", color: "#2a2a2a" },
   // { label: "Clear", color: "#dfdfdf" },
   { label: "Sage", color: "#b1c7a7" },
-  { label: "Gray", color: "#808080" },
+  { label: "Gray", color: "#808080", text: "#fff" },
   { label: "Wood", color: "#cbb294" },
 ]);
