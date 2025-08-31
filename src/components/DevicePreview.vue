c77e569b6ec568552e8a9d2bdf7a8844372a2df5
Gabe Cook <gabe565@gmail.com>
2025-08-31 03:14:29 -0500

    fix: device frame not shrinking on small displays

--- src/components/DevicePreview.vue
+++ src/components/DevicePreview.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useFullscreen } from "@vueuse/core";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import TrmnlLogo from "@/assets/trmnl-logo.svg?raw";
 import { Button } from "@/components/ui/button";
 import { Colors } from "@/sdk/trmnl";
@@ -13,29 +13,25 @@ const props = withDefaults(defineProps<{ imageSrc?: string }>(), { imageSrc: "" 
 
 const screenRef = ref<HTMLImageElement | null>(null);
 const { toggle, isFullscreen } = useFullscreen(screenRef);
-
-const bgStyle = computed(() => ({
-  "background-color": Colors[config.device]?.color,
-}));
 </script>
 
 <template>
   <div
-    class="relative rounded-lg mx-auto max-w-[800px] p-[25px] pt-[24px] pb-[57px] transition-all"
-    :style="bgStyle"
+    class="relative rounded-lg mx-auto max-w-[800px] p-[3.4%] pt-[3.3%] pb-[7.6%] transition-all"
+    :style="{ 'background-color': Colors[config.device]?.color }"
   >
     <Button
       @click="toggle"
-      class="absolute right-0 bottom-0 m-2 inset-shadow-sm/15 shadow-none"
-      :style="bgStyle"
+      class="absolute right-0 bottom-0 m-2 inset-shadow-sm/15 shadow-none bg-white/15 size-6 sm:size-9"
+      size="icon"
     >
       <FullscreenIcon />
     </Button>
     <div
-      class="pointer-events-none absolute bottom-0 left-0 w-full flex justify-center pb-4.5"
+      class="pointer-events-none absolute bottom-0 left-0 w-full flex justify-center pb-[2.4%]"
       aria-hidden="true"
     >
-      <div class="w-28 fill-black opacity-15" v-html="TrmnlLogo" />
+      <div class="w-[15%] fill-black opacity-15" v-html="TrmnlLogo" />
     </div>
     <div
       class="aspect-5/3 w-full bg-[#f7f7f7] rounded-sm overflow-hidden inset-shadow-[0_5px_70px_rgba(0,0,0,0.25)]"
