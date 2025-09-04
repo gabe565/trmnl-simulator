8de068f03dd2598e4f2c12b8bfbe6e585618a8e1
Gabe Cook <gabe565@gmail.com>
2025-09-03 21:06:21 -0500

    refactor: generate an inline svg for the device frame

--- src/components/DevicePreview.vue
+++ src/components/DevicePreview.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useFullscreen } from "@vueuse/core";
-import { computed, ref } from "vue";
+import { computed, useTemplateRef } from "vue";
 import setupLogo from "@/assets/setup-logo.png";
-import TrmnlLogo from "@/assets/trmnl-logo.svg?raw";
+import TrmnlLogo from "@/assets/trmnl-logo.svg";
 import { Button } from "@/components/ui/button";
 import { Colors } from "@/sdk/trmnl";
 import { useConfigStore } from "@/stores/config";
@@ -12,18 +12,72 @@ const config = useConfigStore();
 
 const props = withDefaults(defineProps<{ imageSrc?: string }>(), { imageSrc: "" });
 
-const screenRef = ref<HTMLImageElement | null>(null);
-const { toggle, isFullscreen } = useFullscreen(screenRef);
+const { toggle, isFullscreen } = useFullscreen(useTemplateRef("screen"));
 
 const color = computed(() => Colors[config.device]);
+
+const imageSrc = computed(() => props.imageSrc || setupLogo);
 </script>
 
 <template>
-  <div
-    class="relative rounded-lg mx-auto max-w-[800px] p-[3.4%] pt-[3.3%] pb-[7.6%] transition-all overflow-hidden"
-    :style="{ 'background-color': color?.color }"
-    @dblclick="toggle"
-  >
+  <div class="relative w-full" @dblclick="toggle">
+    <svg viewBox="0 0 850 570">
+      <!-- Background -->
+      <rect
+        x="0"
+        y="0"
+        width="850"
+        height="570"
+        rx="10"
+        ry="10"
+        :fill="color?.color"
+        class="transition-colors"
+      />
+
+      <!-- Logo -->
+      <TrmnlLogo href="#trmnl-logo" x="365" y="522" width="120" height="30" opacity="0.25" />
+
+      <!-- Screen -->
+      <mask id="screen-mask" mask-type="alpha">
+        <rect x="25" y="25" width="800" height="480" rx="5" ry="5" />
+      </mask>
+      <filter id="inset-shadow">
+        <!-- Shadow offset -->
+        <feOffset dx="0" dy="0" />
+        <!-- Shadow blur -->
+        <feGaussianBlur stdDeviation="30" result="offset-blur" />
+        <!-- Invert drop shadow to make an inset shadow -->
+        <feComposite operator="out" in="SourceAlpha" in2="offset-blur" result="inverse" />
+        <!-- Cut color inside shadow -->
+        <feFlood flood-color="black" flood-opacity=".55" result="color" />
+        <feComposite operator="in" in="color" in2="inverse" result="shadow" />
+        <!-- Placing shadow over element -->
+        <feComposite operator="over" in="shadow" in2="SourceGraphic" />
+      </filter>
+      <rect
+        x="25"
+        y="25"
+        width="800"
+        height="480"
+        fill="#f7f7f7"
+        filter="url(#inset-shadow)"
+        mask="url(#screen-mask)"
+      />
+
+      <image
+        ref="screenRef"
+        :href="imageSrc"
+        x="25"
+        y="25"
+        width="800"
+        height="480"
+        class="mix-blend-multiply"
+        mask="url(#screen-mask)"
+      />
+    </svg>
+
+    <img ref="screen" :src="imageSrc" alt="TRMNL Screen" :class="{ hidden: !isFullscreen }" />
+
     <Button
       @click="toggle"
       class="absolute right-0 bottom-0 size-6 sm:size-9 rounded-none rounded-tl-lg"
@@ -34,21 +88,5 @@ const color = computed(() => Colors[config.device]);
     >
       <FullscreenIcon />
     </Button>
-    <div
-      class="pointer-events-none absolute bottom-0 left-0 w-full flex justify-center pb-[2.4%]"
-      aria-hidden="true"
-    >
-      <div class="w-[15%] fill-black opacity-15" v-html="TrmnlLogo" />
-    </div>
-    <div
-      class="aspect-5/3 w-full bg-[#f7f7f7] rounded-sm overflow-hidden inset-shadow-[0_5px_70px_rgba(0,0,0,0.25)] select-none"
-    >
-      <img
-        ref="screenRef"
-        :src="props.imageSrc || setupLogo"
-        alt="TRMNL Screen"
-        :class="{ 'mix-blend-multiply': !isFullscreen }"
-      />
-    </div>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import path from 'node:path'
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import Icons from 'unplugin-icons/vite'
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
+import Icons from "unplugin-icons/vite";
+import { defineConfig } from "vite";
+import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), Icons({ compiler: 'vue3' })],
+  plugins: [vue(), tailwindcss(), svgLoader(), Icons({ compiler: "vue3" })],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
