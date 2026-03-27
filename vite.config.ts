0ea9c93d78c21e7d6ed3e219ccced79796804995
Gabe Cook <git@gabe565.com>
2026-03-26 23:27:17 -0500

    chore(deps): update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 import svgLoader from "vite-svg-loader";
+
+const rootDir = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   plugins: [vue(), tailwindcss(), svgLoader(), Icons({ compiler: "vue3" })],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(rootDir, "./src"),
     },
   },
 });
